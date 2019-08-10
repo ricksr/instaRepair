850cd5a995e26d88cea6657c71279270c70351b8
ricskr <sutirtharej@gmail.com>
2019-08-10 19:22:41 +0530

    new changes

--- main.py
+++ main.py
@@ -88,5 +88,6 @@ def home():
  return render_template('home.html')
 
 
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+   port = int(os.environ.get("PORT", 5000))
+   app.run(debug=True, port=port)
